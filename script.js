4e62e4da15eb3b4cc9a97003209759f18c8e9a15
jamshidbek455325 <muhammadsolihnikanali00@gmail.com>
2025-03-09 08:11:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function salomBer() {
+    alert("Assalomu alaykum! Veb-saytingiz ishlayapti 🎉riimiz haqida Salom! bu sitega men o'z dizaynlarimni joylab boraman. Siz bu dizanlarni yoqqanini tanlab Design-(dizan rapami) ni  abvca698@gmail.com  ga aytsez sizga moq qilib yasab beraman");
+}
